1d32cdf7ead38aa465cdd10ef7eb4fc952f91b8c
Mallikarjun Vani <149805951+MallikarjunVani28@users.noreply.github.com>
2026-05-29 23:06:15 +0530

    Add module for right-angled triangle pattern generation

---
+++ pattern_generation/02.right_angeled_triangle.v
@@ -0,0 +1,11 @@
+module square_pattern;
+  integer i,j;
+  initial begin
+    for(i= 0;i<5;i=i+1)begin
+      for (j = 0;j<=i;j=j+1)begin
+        $write("* ");
+      end
+      $write("\n");
+    end
+  end
+endmodule

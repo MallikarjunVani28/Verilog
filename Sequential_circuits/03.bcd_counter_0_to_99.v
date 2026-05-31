22577af531a64943a264fe836030bcacd3c14c91
Mallikarjun Vani <149805951+MallikarjunVani28@users.noreply.github.com>
2026-05-31 09:58:29 +0530

    Add BCD counter module for 0 to 99

---
+++ Sequential_circuits/03.bcd_counter_0_to_99.v
@@ -0,0 +1,26 @@
+module bcd_counter(input clk,rst,output reg [3:0]q1,q2);
+  always@(posedge clk)begin
+    if(rst)
+      q1 <= 1'b0;
+    else if(q1 == 4'b1001)
+      q1 <= 1'b0;
+    else
+      q1 <= q1 + 1'b1;
+  end
+  
+  always@(posedge clk)begin
+    if(rst)
+      q2 <= 0;
+    
+    
+    else if(q2 == 4'b1001 && q1 == 4'b1001)
+      q2 <= 4'b0000;
+     else if(q1 == 4'b1001 )
+      q2<= q2+1'b1;
+   
+      end
+endmodule
+      
+    
+    
+      

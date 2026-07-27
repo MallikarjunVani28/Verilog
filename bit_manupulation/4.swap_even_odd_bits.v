3057767765181e6487eaa0af4a922060da638659
Mallikarjun Vani <149805951+MallikarjunVani28@users.noreply.github.com>
2026-07-27 13:01:48 +0530

    Fix bit manipulation by swapping left and right shifts

--- bit_manupulation/4.swap_even_odd_bits.v
+++ bit_manupulation/4.swap_even_odd_bits.v
@@ -5,5 +5,5 @@ module swap_bits(input [7:0] data ,output [7:0] out);
   assign even_bits = data & 8'h55;
   assign odd_bits = data & 8'haa;
   
-  assign out = (even_bits >>1 )|(odd_bits<<1);
+  assign out = (even_bits <<1 )|(odd_bits>>1);
 endmodule
